c0547e2eec3b3e9c09cf7d94e68f178883040b4b
zompina <72997538+zompina@users.noreply.github.com>
2021-08-26 21:00:33 +0400

    Add files via upload
    adding file from the Computer

---
+++ ConditionalOpreration.py
@@ -0,0 +1,14 @@
+again = "y"
+while again == "y":
+    num1 = int(input("Enter Number 1: "))
+    num2 = int(input("Enter Number 2: "))
+    big = True if num1>num2 else False
+    print(big)
+    again = input("Do you want to try again (y/n)?")
+
+
+
+#num1 = int(input("Enter Number 1: "))
+#num2 = int(input("Enter Number 2: "))
+#big = True if num1>num2 else False
+#print(big)
